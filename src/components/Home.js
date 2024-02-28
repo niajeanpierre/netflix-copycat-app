6f03a81ee1d839333254424870c5cdf69563e2d8
niajeanpierre <nia.jeanpierre@mykenzie.snhu.edu>
2024-02-28 10:45:02 -0500

    added movie api and material ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function Home() {
-  return (
-    <div>Home</div>
-  )
+function Home () {
+  const getMovie = () => {
+    try {
+      fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=603e12b90b1dcd1e0df21be534092ac4'
+      )
+        .then(res => res.json())
+        .then(json => console.log(json.results))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getMovie()
+  }, [])
+
+  return
+  ;<div>Home</div>
 }
 
 export default Home
