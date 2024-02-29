088e9e53438941e740601bb400251158b92fe84f
niajeanpierre <nia.jeanpierre@mykenzie.snhu.edu>
2024-02-29 08:53:56 -0500

    added featured movie annd buttons to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import netflixLogo from "../images/netflixLogo.png"
+import netflixLogo from '../images/netflixLogo.png'
 import { Button } from '@mui/material'
 
-function NavBar() {
+function NavBar () {
   const [movies, setMovies] = useState([])
 
   /**
@@ -28,10 +28,37 @@ function NavBar() {
   console.log(movies[0])
 
   return (
-    <div style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movies[4]?.poster_path})`, backgroundPosition:"center", backgroundRepeat:"no-repeat", backgroundSize:"cover", height:"400px", width:"100%"}}>
-      <img style={{width:"110px", height: "90px"}} src={netflixLogo}/>
-      <Button>SignIn</Button>
+    <div
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${movies[6]?.poster_path})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '400px',
+        width: '100%'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '20px'
+        }}
+      >
+        <img style={{ width: '90px', height: '90px' }} src={netflixLogo} />
+        <Button color='error' variant='contained' sx={{ height: '40px' }}>
+          Sign In
+        </Button>
       </div>
+      <div style={{padding: "20px"}}>
+        <h1 style={{color:"#F4F4F4", fontSize:"70px", fontFamily:"initial"}}>{movies[6]?.original_title}</h1>
+        <h3 style={{color:"#F4F4F4", }}>
+          {movies[6].overview}
+        </h3>
+        <Button variant='contained' sx={{color:"black", bgcolor: "white", fontWeight:"bold"}}>View Trailer</Button>
+      </div>
+     
+    </div>
   )
 }
 
